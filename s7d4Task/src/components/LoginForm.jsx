1b675a068468c46e8e0507afe135875c7bd0da07
zehraBal <fz.bal00@gmail.com>
2024-07-03 16:56:59 +0300

    Form validations complete, success page complete

--- s7d4Task/src/components/LoginForm.jsx
+++ s7d4Task/src/components/LoginForm.jsx
@@ -1,14 +1,20 @@
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function LoginForm() {
   const navigate = useNavigate();
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
+    getValues,
   } = useForm({
-    defaultValues: { email: "", password: "", check: false },
+    defaultValues: { email: "", password: "", checkbox: false },
+    mode: "all",
   });
+
   const formSubmit = (formData) => {
     console.log(formData);
     axios
@@ -19,6 +25,16 @@ export default function LoginForm() {
       })
       .catch((err) => console.warn(err));
   };
+
+  const validatePassword = (value) => {
+    return (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+        value
+      ) ||
+      "'Password must contain at least 8 characters, including one uppercase letter, one lowercase letter, one number, and one special character."
+    );
+  };
+
   return (
     <div className="loginForm">
       <div className="formArea">
@@ -26,14 +42,48 @@ export default function LoginForm() {
           <h3>LOGIN FORM</h3>
 
           <label htmlFor="email">Email:</label>
-          <input type="email" />
+          <input
+            type="email"
+            {...register("email", {
+              required: "E-mail field cannot be left blank",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Please enter a valid e-mail address.",
+              },
+            })}
+            placeholder="Enter your email"
+          />
+          {errors.email && (
+            <p style={{ color: "red" }}>{errors.email.message}</p>
+          )}
+
           <label htmlFor="password">Password:</label>
-          <input type="password" />
+          <input
+            type="password"
+            {...register("password", {
+              validate: validatePassword,
+            })}
+            placeholder="Enter your password"
+          />
+          {errors.password && (
+            <p style={{ color: "red" }}>{errors.password.message}</p>
+          )}
+
           <div className="formCheckbox">
-            <input type="checkbox" id="checkbox1" />
-            <label htmlFor="checkbox1">Şartları kabul ediyorum</label>
+            <input
+              type="checkbox"
+              {...register("checkbox", {
+                required: "You must accept the terms",
+              })}
+              defaultChecked={false}
+            />
+            <label htmlFor="checkbox">Şartları kabul ediyorum</label>
           </div>
-          <button type="submit" className="loginBtn">
+          {errors.checkbox && (
+            <p style={{ color: "red" }}>{errors.checkbox.message}</p>
+          )}
+
+          <button type="submit" disabled={!isValid} className="loginBtn">
             Login
           </button>
         </form>
--- s7d4Task/src/components/Success.jsx
+++ s7d4Task/src/components/Success.jsx
@@ -1,7 +1,15 @@
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
+    <div className="successPage">
+      <h1>Login Successfull !!! </h1>
+      <div>
+        <video
+          src="/src/assets/welcome.mp4"
+          width="320"
+          height="240"
+          controls
+        ></video>
+      </div>
     </div>
   );
 }
